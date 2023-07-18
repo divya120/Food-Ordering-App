88e71665fde5c69e0cc1a4be7fb6b01ed669d1f4
Divya Kulkarni <divya12.7a@gmail.com>
2023-07-18 17:26:34 +0530

    Used Swiggy API to fetch data

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,13 +1,30 @@
 import RestaurantCards from "./RestaurantCard";
 import swiggyData from "../utils/swiggy.json"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const resParsed = swiggyData.data.cards[2].data.data.cards;
 
 
 
 const Body = () => {
-  const [listOfRestaurants, setlistOfRestaurant] = useState(resParsed)
+  const [listOfRestaurants, setlistOfRestaurant] = useState([])
+
+  useEffect(() => {
+    fetchData()
+  },[])
+
+  const fetchData = async() =>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.1202009&lng=72.8392592&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    console.log(json?.data?.cards[2]?.data?.data)
+    setlistOfRestaurant(json?.data?.cards[2]?.data?.data.cards)
+  }
+
+
+
+  
+  
+  
     return (
       <div className="body">
         <div className="search-ctn">
