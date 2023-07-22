51f2d2e93a7687c3c6eb3f5516da0ad2774fb331
Divya Kulkarni <divya12.7a@gmail.com>
2023-07-22 14:16:59 +0530

    Added links to the navbar 🛠

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import { LOGO_URL } from "../utils/constants";  
 import { useEffect, useState } from "react";
+import {Link} from 'react-router-dom'
 
 
 const Header = () => {
@@ -17,9 +18,9 @@ const Header = () => {
 
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contact Us</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About us</Link></li>
+          <li><Link to="/contact">Contact Us</Link></li>
           <li>Cart</li>
           <li> <button className="login-btn" onClick={(event)=>{
           btnName==="Login"?setbtnName("Logout"):setbtnName("Login");
